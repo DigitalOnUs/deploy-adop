15285274c0b5bd1d9d8dcedc21210b33cf2aef02
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-06-15 15:39:42 -0500

    Create deploy/destroy Bash scripts.

---
+++ params.sh
@@ -0,0 +1,10 @@
+export AWS_ACCESS_KEY_ID=
+export AWS_ACCESS_SECRET_KEY=
+export AWS_DEFAULT_REGION=us-west-2
+export AWS_INSTANCE_TYPE=m4.2xlarge
+export AWS_VPC_ID=
+export AWS_SPOT_PRICE=0.2
+export ADOP_DIR=~/code/deployed-adop-docker-compose
+export ADOP_USER=uadop
+export ADOP_PWD=padop123
+export ADOP_COMMIT=d32cfa98ac9574a7c400fd37e927b52adc6fc7b9
